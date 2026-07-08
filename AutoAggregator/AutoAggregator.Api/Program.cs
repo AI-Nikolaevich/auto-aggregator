a2aed7e73d7d084cad144afa92c6b939967cb25c
AI-Nikolaevich <ilia.antukhov@yandex.ru>
2026-07-08 11:53:27 +0300

    bootstrap-api

--- AutoAggregator/AutoAggregator.Api/Program.cs
+++ AutoAggregator/AutoAggregator.Api/Program.cs
@@ -1,23 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = string.Empty;
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "AutoAggregator API v1");
+    });
 }
 
 app.UseHttpsRedirection();
-
+app.MapGet("/api", () => "hello world");
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
